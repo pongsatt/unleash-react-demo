fd920ca3cd9c6fbf1a2fd6e7a16a0bafb2bf5d83
pongsatt <pongsatt@gmail.com>
2022-08-27 17:12:26 +0700

    simplify button

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -1,7 +1,15 @@
-import './MyButton.css';
-
 export default function MyButton() {
     return (
-        <button>Button</button>
+        <button style={{
+            border: 'none',
+            color: 'black',
+            padding: '15px 32px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            margin: '4px 2px',
+            cursor: 'pointer',
+        }} >Button</button >
     );
 }
